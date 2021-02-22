53d9412a9c15dd1db18e9ffc2ca01da56c278aeb
Dejan Kuzmanovic <dejan.kuzmanovic@yahoo.com>
2021-02-22 15:53:51 +0100

    Create Spring Boot app using Spring Initializr

---
+++ src/test/java/com/eurobasket/pmf/PmfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurobasket.pmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
